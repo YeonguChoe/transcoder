1416311b238e25bc9ea93db10677af779d78f7a9
YeonguChoe <yeongu.choe@icloud.com>
2025-11-17 22:10:28 -0500

    demuxer

--- main.cpp
+++ main.cpp
@@ -8,33 +8,34 @@ extern "C"
 using namespace std;
 int main(int argc, char *argv[])
 {
-    // FFmpeg
+    ///////////////////////// Demux using FFmpeg /////////////////////////
+    // FFmpeg Demux
     const char *filename = argv[1];
     AVFormatContext *formatContext = nullptr;
+
+    // open input file
     avformat_open_input(&formatContext, filename, nullptr, nullptr);
+
+    // find stream
     avformat_find_stream_info(formatContext, nullptr);
     av_dump_format(formatContext, 0, filename, 0);
 
-    int videoStreamIndex = -1;
-    for (int i = 0; i < formatContext->nb_streams; i++)
-    {
-        if (formatContext->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
-        {
-            videoStreamIndex = i;
-            break;
-        }
-    }
-
-
-    AVStream *videoStream = formatContext->streams[videoStreamIndex];
+    // video stream
+    AVStream *videoStream = formatContext->streams[0];
     AVCodecParameters *codecParams = videoStream->codecpar;
 
+    // video codec
     const AVCodec *codec = avcodec_find_decoder(codecParams->codec_id);
     AVCodecContext *codecContext = avcodec_alloc_context3(codec);
     avcodec_parameters_to_context(codecContext, codecParams);
-    avcodec_open2(codecContext, codec, nullptr);
+
+    ///////////////////////// Decode using Nvidia /////////////////////////
 
 
+    ///////////////////////// Free FFmpeg resource /////////////////////////
+    // free FFmpeg resource
+    avcodec_free_context(&codecContext);
+    avformat_close_input(&formatContext);
 
     return 0;
 }
